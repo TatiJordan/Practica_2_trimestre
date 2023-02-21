dc16ab320a917ec1ee0160bacb18434e5417513e
TatiJordan <jordan_perez.tatiana@tiernoparla.es>
2023-02-21 21:54:50 +0100

    Clase ObrasDeArte con variables

---
+++ src/main/java/tiernoparla/es/ObrasDeArte.java
@@ -0,0 +1,14 @@
+public class ObrasDeArte {
+    
+    private static ObrasDeArte[] obras;
+
+    private int id;
+    private String nombre;
+    private String autor;
+    private String tipo;
+    private double precio;
+    private double alturaM;
+    private double peso;
+    private int piezas;
+    private String descripcion;
+}//ObrasDeArte
