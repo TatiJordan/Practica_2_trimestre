7ec531a226723667191e6f7d3d1e8ed3a5f92d8d
TatiJordan <jordan_perez.tatiana@tiernoparla.es>
2023-02-21 22:51:45 +0100

    Constructores de las clases Escultura y Pintura

--- src/main/java/tiernoparla/es/Escultura.java
+++ src/main/java/tiernoparla/es/Escultura.java
@@ -1,4 +1,10 @@
 public class Escultura extends ObrasDeArte {
 
     private String material;
+
+    public Escultura(int id, String nombre, String autor, String tipo, String material,
+            double precio, double alturaM, double pesoT, int piezas, String descripcion) {
+        super(id, nombre, autor, tipo, precio, alturaM, pesoT, piezas, descripcion);
+        this.material = material;
+    }//constructor
 }//escultura
--- src/main/java/tiernoparla/es/Pintura.java
+++ src/main/java/tiernoparla/es/Pintura.java
@@ -1,6 +1,13 @@
 package tiernoparla.es;
 
 public class Pintura extends ObrasDeArte{
+    
     private String tecnica;
+
+    public Pintura(int id, String nombre, String autor, String tipo, String tecnica,
+            double precio, double alturaM, double pesoT, int piezas, String descripcion) {
+        super(id, nombre, autor, tipo, precio, alturaM, pesoT, piezas, descripcion);
+        this.tecnica = tecnica;
+    }// constructor
 
 }//Pintura
