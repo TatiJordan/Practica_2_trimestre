4c65c8bb1ab1bbe9b0e89b2df639bba75f29428f
TatiJordan <jordan_perez.tatiana@tiernoparla.es>
2023-02-21 23:32:54 +0100

    Test añadirObra

--- src/test/java/tiernoparla/es/Test.java
+++ src/test/java/tiernoparla/es/Test.java
@@ -17,4 +17,20 @@ public class Test {
         assertFalse(ObrasDeArte.getObras() == null);
 
     }// comprobar array Test
+
+    // Probar que se añade una obra sin problemas y al final del array
+    @Test
+    void añadirObra() {
+
+        int posicionPintura = 5;
+        int idPintura = 6;
+        ObrasDeArte pintura = new Pintura(idPintura, "pintura", "cesar", "escultura", "oleo", 50, 80, 100, 5, "desc1");
+
+        ObrasDeArte.añadirObra(pintura);
+
+        assertFalse(ObrasDeArte.getObras()[posicionPintura] == null);
+
+        assertEquals(idPintura, ObrasDeArte.getObras()[posicionPintura].getId());
+
+    }// añadir obras Test
 }//Test
