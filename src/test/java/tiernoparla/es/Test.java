153d7ac5177dc5d01294cf8c61246cfc8f8dc049
TatiJordan <jordan_perez.tatiana@tiernoparla.es>
2023-02-21 23:30:52 +0100

    Creamos una clase de Test y ponemos los BeforeEach

---
+++ src/test/java/tiernoparla/es/Test.java
@@ -0,0 +1,14 @@
+package tiernoparla.es;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Test {
+    
+    @BeforeEach
+    static void crearArray() {
+
+        ObrasDeArte.crearArray();
+    }// crear array
+
+}//Test
